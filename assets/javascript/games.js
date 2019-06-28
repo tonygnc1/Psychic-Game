5f73c6e18d2bc5e3e2344245f1fcb84d299faea3
tonygnc1 <bootcamp1955@gmail.com>
2019-06-28 16:44:12 -0400

    Final Push - Finished Product

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,30 +1,40 @@
-{/* <script type="javascript"> */}
-
+var guessesLeft;
+var soFar;
+var computerGuess;
 var wins = 0;
 var losses = 0;
-var guessesLeft = 20;
-var soFar = 0;
-
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
-"o","p","q","r","s","t","u","v","w","x","y","z"];
-
-document.onkeyup = function() {
-     var userGuess = String.fromCharCode(event.keyCode), 
-     toLowerCase();
-
-     
-     var computerGuess = letters[Math.floor(Math.random()*letters.length)];
 
 
-if (userGuess === computerGuess) {
-    document.getElementById("wins").textContent = wins++;
-    soFar = []
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    if (guessesLeft <= 0) {
-        document.getElementById("losses").textContent = losses++;
-        document.getElementById("guessesLeft").textContent = guessesLeft--;
-    }   
+function startNewGame() {
+    guessesLeft = 6;
+    soFar = [];
+    computerGuess = letters[Math.floor(Math.random() * letters.length)];
 }
 
+startNewGame()
+console.log(computerGuess)
+document.onkeyup = function () {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-// </script>
+    console.log(userGuess);
+    soFar.push(userGuess)
+    document.getElementById("soFar").textContent = soFar;
+    console.log(soFar)
+    if (userGuess === computerGuess) {
+        document.getElementById("wins").textContent = ++wins;
+        startNewGame()
+    }
+
+    document.getElementById("guessesLeft").textContent = --guessesLeft;
+
+    if (guessesLeft <= 0) {
+        document.getElementById("losses").textContent = ++losses;
+        startNewGame()
+    }
+
+
+
+}
