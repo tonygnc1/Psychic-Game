1ddffd5ded539b531e4909aecd959299b22bf8b4
tonygnc1 <bootcamp1955@gmail.com>
2019-07-08 20:14:29 -0400

    Cleaned up pseudocode

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -4,16 +4,17 @@ var computerGuess;
 var wins = 0;
 var losses = 0;
 var usedLetters = [];
-var userInput = userInput;
-var checkGuess = 0;
+var userInput;
+
 
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
     "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 function startNewGame() {
+    usedLetters = [];
     guessesLeft = 6;
-    soFar = [];
+    document.getElementById("soFar").textContent = usedLetters;
     computerGuess = letters[Math.floor(Math.random() * letters.length)];
 }
 
@@ -23,41 +24,41 @@ startNewGame()
 console.log(computerGuess)
 document.onkeyup = function () {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userGuess);
 
-    // New user guess
-    soFar.push(userGuess)
-    document.getElementById("soFar").textContent = soFar;
-    console.log(soFar)
+    console.log(usedLetters)
 
-    // Code to keep user from repeating same guess 
-if (userGuess === (usedLetters)) {
-
-    // if (letters.includes(userInput)) {
-    //     if(usedLetters.includes(userInput))
-        document.getElementById("userGuess").innerHTML = "";
-        console.log(usedLetters);
-        alert("You have repeated a guess");
+    if (usedLetters.includes(userGuess)) {
+        alert("You have chosen the same letter, try again!");
     }
-
-// Compare user guess to computer guess, if user guess is equal 
-// to computer guess, add to wins
-    if (userGuess === computerGuess) {
-        document.getElementById("wins").textContent = ++wins;
+    // Compare user guess to computer guess, if user guess is equal 
+    // to computer guess, add to wins
+    else if (userGuess === computerGuess) {
+        wins++;
+        document.getElementById("wins").textContent = wins;
         startNewGame()
         console.log(wins)
+    } else {
+    // If user uses all gueses, add to losses
+        usedLetters.push(userGuess)
+        document.getElementById("soFar").textContent = usedLetters;
+        guessesLeft--;
+        if (guessesLeft <= 0) {
+            losses++;
+            document.getElementById("losses").textContent = losses;
+            startNewGame()
+            console.log(losses)
+        }
+         // How many guesses user has left
+        document.getElementById("guessesLeft").textContent = guessesLeft;
+        console.log(guessesLeft)
     }
-// How many guesses user has left
-    document.getElementById("guessesLeft").textContent = --guessesLeft;
-console.log(guessesLeft)
 
-// If user uses all gueses, add to losses
-    if (guessesLeft <= 0) {
-        document.getElementById("losses").textContent = ++losses;
-        startNewGame()
-        console.log(losses)
-    }
+
+   
 
 
 
 }
+
+
+
